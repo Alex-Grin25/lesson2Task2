ca64185e262e9dd8cdbf8262e2035f9c85a096f8
Alex-Grin25 <lona.ai@yahoo.com>
2021-05-27 14:56:18 +0300

    Initial Commit

---
+++ lesson2task2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  lesson2task2
+//
+//  Created by ALEKSANDR GRIGOREV on 27.05.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
